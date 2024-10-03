2480d92c288ad0f9d75a1a8e4796d3a3031b34e9
augusto.oliveira <augusto.oliveira@coopersystem.com.br>
2024-10-03 11:46:37 -0300

    chore: added host

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0bo4u+pq71(n2+%!@6w2f3tb#$^!j)@c0jmrg1$!rsi#*1(*m7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '147.79.82.119']
 
 
 # Application definition
