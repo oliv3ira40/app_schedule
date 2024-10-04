abb9ba12eaf95ff18c28958a4f8c75c98de57a3f
augusto.oliveira <augusto.oliveira@coopersystem.com.br>
2024-10-04 11:45:13 -0300

    chore: validates when there is no data

--- admin_interface/views.py
+++ admin_interface/views.py
@@ -29,35 +29,13 @@ def dashboard_callback(request, context):
     count_unfin_packages_prof = Package.get_count_unfin_packages_by_prof(request)
     count_unfin_treatments_prof = Treatment.get_count_unfin_treatments_by_prof(request)
     count_unfinished_services = count_unfin_packages_prof+count_unfin_treatments_prof
-
-    # scheduled_sessions = Session.get_scheduled_sessions()
+    
     context.update(
         {
             # "sample": "example",  # this will be injected into templates/admin/index.html
             "sample": "templates/admin/index.html",
-            "current_user": current_user,
-            "upcoming_birthdays_table": {
-                "headers": ["Cliente", 	"Telefone", "Data do aniversário", "Idade atual"],
-                "rows": [
-                    [
-                        client.name,
-                        client.phone,
-                        client.formt_birth_date,
-                        client.age_at_next_birthday,
-                    ] for client in upcoming_birthdays
-                ]
-            },
-            "upcoming_appointments_table": {
-                "headers": ["Agendada para o dia", "Tipo", "Cliente", "Serviço"],
-                "rows": [
-                    [
-                        appointment.date,
-                        appointment.type,
-                        appointment.client.name,
-                        appointment.service.name,
-                    ] for appointment in upcoming_appointments
-                ]
-            },
+            "upcoming_birthdays": upcoming_birthdays,
+            "upcoming_appointments": upcoming_appointments,
             'count_clients_prof' : count_clients_prof,
             'count_finished_services' : count_finished_services,
             'count_unfinished_services' : count_unfinished_services,
