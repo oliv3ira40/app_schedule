62191fd7aa739f3cee1d6ceec3f688fb56adc33f
augusto.oliveira <augusto.oliveira@coopersystem.com.br>
2024-10-07 11:09:57 -0300

    chore: start install website theme

--- static/website/js/_main.js
+++ static/website/js/_main.js
@@ -1,3 +1,7 @@
-// Os arquivos que serão minificados devem ser importados aqui
-
-// import './seu-arquivo.js';
+import './site/retina-1.1.0.min.js';
+import './site/jquery.easing.js';
+import './site/menu.js';
+import './site/tipper.js';
+import './site/scrollReveal.js';
+import './site/fitvids.js';
+import './site/custom-blog.js';
