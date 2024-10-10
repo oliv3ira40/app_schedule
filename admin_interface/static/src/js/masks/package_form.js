07ab2ef999f7fb84a58ed5aacb0e832b670ae691
augusto.oliveira <augusto.oliveira@coopersystem.com.br>
2024-10-09 21:33:27 -0300

    chore: added session masks

--- admin_interface/static/src/js/masks/package_form.js
+++ admin_interface/static/src/js/masks/package_form.js
@@ -7,11 +7,20 @@ $(function() {
         input_value.attr('type', 'text');
         input_value.mask('00.000.000,00', {reverse: true});
         
-        var date = package_form.find('.field-date');
-        var input_date = date.find('input.vDateField');
-        var input_time = date.find('input.vTimeField');
-        input_date.mask('00/00/0000');
-        input_time.mask('00:00:00');
+        init_masks_date_field()
+
+        var clear = setInterval(function() {
+            var add_row = $('.add-row');
+            if (add_row.length) {
+                clearInterval(clear);
+                var link = add_row.find('a');
+                link.on('click', function() {
+                    setTimeout(function() {
+                        init_masks_date_field();
+                    }, 100);
+                });
+            }
+        }, 100);
 
         // Remova a formatação antes de enviar o formulário
         package_form.on('submit', function() {
@@ -19,5 +28,13 @@ $(function() {
             var numeric_value = formatted_value.replace(/\./g, '').replace(',', '.');
             input_value.val(numeric_value);
         });
+
+        function init_masks_date_field() {
+            var date = package_form.find('.field-date');
+            var input_date = date.find('input.vDateField');
+            var input_time = date.find('input.vTimeField');
+            input_date.mask('00/00/0000');
+            input_time.mask('00:00:00');
+        }
     }
 })
